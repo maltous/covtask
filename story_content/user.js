caa72592b6c17fc523154e7382904b541d8685af
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-17 13:17:45 +0300

    covtask

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6mEBq8UqfvM":
+        Script1();
+        break;
+      case "6LOXzcquzHW":
+        Script2();
+        break;
+      case "6qKzoJ7QsRj":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  window.print();
+}
+
